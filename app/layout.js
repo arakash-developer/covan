a975675e8dcbf261e7685b308804b56f82c778b5
akash100a <arakash968786@gmail.com>
2025-01-27 04:44:50 +0600

    update

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-import { Mali } from "next/font/google";
+import {Mulish } from "next/font/google";
 import "./globals.css";
 
-const malifont = Mali({
+const mulish = Mulish({
   weight: ["200", "300", "400", "500", "600", "700"],
   subsets: ["latin"],
 });
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${malifont.className}`}
+        className={`${mulish.className}`}
       >
         {children}
       </body>
