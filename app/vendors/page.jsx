c8531fd85f475a7d284fe90c07c9b02ccfde0ee7
akash100a <arakash968786@gmail.com>
2025-02-02 11:56:20 +0600

    update

--- app/vendors/page.jsx
+++ app/vendors/page.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import Vendor1 from "@/public/vendor1.png";
+import VendorLogo1 from "@/public/vendorlogo1.jpg";
+import Image from "next/image";
+import Container from "../component/Container";
 
-const page = () => {
+const page = ({ vendorimg = Vendor1, VendorLogo = VendorLogo1 }) => {
   return (
-    <div>page</div>
-  )
-}
+    <div>
+      <Container>
+        <div className="w-[370px]">
+          <div className="w-full h-[220px] overflow-hidden">
+            <Image
+              src={vendorimg}
+              alt={vendorimg}
+              className="w-full h-full bg-cover"
+            />
+          </div>
+          <div className="w-full px-5 py-4 bg-[#fff] "></div>
+        </div>
+      </Container>
+    </div>
+  );
+};
 
-export default page
+export default page;
