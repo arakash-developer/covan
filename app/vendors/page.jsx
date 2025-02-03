16a4ffe4b61541690f5991d0ccc1ade6b351ee63
akash100a <arakash968786@gmail.com>
2025-02-03 16:03:46 +0600

    update

--- app/vendors/page.jsx
+++ app/vendors/page.jsx
@@ -1,5 +1,6 @@
 import Vendor1 from "@/public/vendor1.png";
 import VendorLogo1 from "@/public/vendorlogo1.jpg";
+import { IoFilterSharp } from "react-icons/io5";
 import Container from "../component/Container";
 import VendorCard from "../component/layers/VendorCard";
 
@@ -13,6 +14,25 @@ const page = ({
   return (
     <div>
       <Container className="mt-[110px] mb-[150px]">
+        <div className="mb-5 py-3 h-[74px] flex justify-between items-center brdr">
+          <p className="font-normal text-sm leading-[175%] text-[#666]">
+            Total stores showing: 6
+          </p>
+          <div className="flex items-center gap-[30px] h-full ">
+            <div className="h-full px-5 bg-[#e7b053] flex items-center gap-1">
+              <IoFilterSharp className="font-bold text-[18px] leading-[175%] text-[#fff]" />
+              <p className="font-bold text-base leading-[175%] tracking-[3px] uppercase text-center text-[#fff]">
+                Filter
+              </p>
+            </div>
+            <div className="h-full flex items-center ">
+              <p className="font-normal text-sm leading-[175%] text-[#666]">
+                Sort by:
+              </p>
+              <p className="font-normal text-sm leading-[175%] text-[#000] border-2 border-[#edecec] px-5 h-full flex items-center justify-center">Most Recent</p>
+            </div>
+          </div>
+        </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-[30px]">
           <VendorCard />
           <VendorCard />
