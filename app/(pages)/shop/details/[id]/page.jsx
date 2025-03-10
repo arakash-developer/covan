1e82b02ea6f09223702fc7c37cd37c7a0b8decb6
akash100a <arakash968786@gmail.com>
2025-03-10 22:31:54 +0600

    Enhance shop details page with layout and navigation elements

--- app/(pages)/shop/details/[id]/page.jsx
+++ app/(pages)/shop/details/[id]/page.jsx
@@ -1,7 +1,35 @@
+import { Prata } from "next/font/google";
+import { FaAngleRight } from "react-icons/fa";
+
+const Pratafont = Prata({
+  weight: "400",
+  subsets: ["latin"],
+});
+
 const page = async ({ params }) => {
-    let { id } = await params;
-    return <div>page {id}</div>;
-  };
-  
-  export default page;
-  
+  let { id } = await params;
+  return (
+    <>
+      {/* Shop */}
+      <section className="flex justify-center items-center flex-col py-[120px] bg-shop bg-cover bg-no-repeat bg-center">
+        <h2
+          className={`font-normal text-3xl leading-[120%] uppercase text-center text-[#fff] ${Pratafont.className}`}
+        >
+          Shop
+        </h2>
+        <div className="flex items-center gap-2">
+          <p className="font-normal text-[0.81rem] leading-[176%] text-[#fff]">
+            Home
+          </p>
+          <FaAngleRight className="font-normal text-[0.81rem] leading-[176%] text-[#fff]" />
+          <p className="font-normal text-[0.81rem] leading-[179%] text-[#e7b053]">
+            Shop
+          </p>
+        </div>
+      </section>
+      {/* Shop */}
+    </>
+  );
+};
+
+export default page;
