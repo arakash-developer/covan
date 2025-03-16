1a8e309c0455bd15b8796c8e229f5d34a5b7e2a3
akash100a <arakash968786@gmail.com>
2025-03-16 13:53:02 +0600

    Add product description and additional details to product details page

--- app/(pages)/shop/details/[id]/page.jsx
+++ app/(pages)/shop/details/[id]/page.jsx
@@ -172,6 +172,71 @@ const page = async ({ params }) => {
                 More Products
               </h3>
             </div>
+            <div className="mt-10">
+              <p className="font-normal text-sm leading-[200%] text-[#666]">
+                Curabitur egestas malesuada volutpat. Nunc vel vestibulum odio,
+                ac pellentesque lacus. Pellentesque dapibus nunc nec est
+                imperdiet, a malesuada sem rutrum. Sed quam odio, porta a
+                finibus quis, sagittis aliquet leo. Nunc ornare metus urna, eu
+                luctus velit placerat ut. Cras at porttitor lectus. Ut dapibus
+                aliquam nibh, in imperdiet libero tincidunt sit amet. Morbi
+                sodales fermentum nibh nec facilisis. Morbi pharetra varius
+                velit, eget varius libero finibus quis. Quisque auctor varius
+                lectus, lacinia rhoncus velit posuere vel. Cras condimentum
+                tincidunt urna, sed vehicula ipsum dapibus et. Pellentesque
+                pharetra ultrices varius. Sed viverra nec purus ut ornare.
+              </p>
+              <div className="mt-5">
+                <div className="flex gap-2 items-center">
+                  <p className="font-normal text-sm leading-[200%] text-[#e7b053]">
+                    N
+                  </p>
+                  <p className="font-normal text-sm leading-[200%] text-[#666]">
+                    Aenean auctor sem ac ex efficitur
+                  </p>
+                </div>
+                <div className="flex gap-2 items-center">
+                  <p className="font-normal text-sm leading-[200%] text-[#e7b053]">
+                    N
+                  </p>
+                  <p className="font-normal text-sm leading-[200%] text-[#666]">
+                    Non mattis odio bibendum
+                  </p>
+                </div>
+                <div className="flex gap-2 items-center">
+                  <p className="font-normal text-sm leading-[200%] text-[#e7b053]">
+                    N
+                  </p>
+                  <p className="font-normal text-sm leading-[200%] text-[#666]">
+                    Sed vitae enim at tortor finibus
+                  </p>
+                </div>
+                <div className="flex gap-2 items-center">
+                  <p className="font-normal text-sm leading-[200%] text-[#e7b053]">
+                    N
+                  </p>
+                  <p className="font-normal text-sm leading-[200%] text-[#666]">
+                    Integer facilisis eleifend vehicula
+                  </p>
+                </div>
+                <div className="flex gap-2 items-center">
+                  <p className="font-normal text-sm leading-[200%] text-[#e7b053]">
+                    N
+                  </p>
+                  <p className="font-normal text-sm leading-[200%] text-[#666]">
+                    In hac habitasse platea dictumst
+                  </p>
+                </div>
+              </div>
+              <p className="font-normal text-sm leading-[200%] text-[#666] mt-3">
+                Sed molestie orci sem, at semper est molestie ac. Suspendisse
+                cursus feugiat erat, eu posuere massa. Nullam posuere nibh non
+                eros lobortis tempus. Maecenas dignissim elementum massa, vel
+                accumsan urna elementum in. Suspendisse at dui euismod, rhoncus
+                eros non, imperdiet ipsum. Vestibulum vehicula vel turpis et
+                vestibulum. Ut porta et ex maximus malesuada.
+              </p>
+            </div>
           </div>
         </Container>
       </section>
