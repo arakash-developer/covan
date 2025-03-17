f2720b72e7b1c018eaaac100273a7cc76e34e664
akash100a <arakash968786@gmail.com>
2025-03-18 03:48:02 +0600

    Add horizontal scroll to Cart table for better product display

--- app/(pages)/cart/page.jsx
+++ app/(pages)/cart/page.jsx
@@ -37,83 +37,85 @@ const page = () => {
       </section>
       {/* Shop */}
       <Container className="mt-[110px] mb-[150px] flex md:flex-row flex-col justify-between items-start">
-        <table className="min-w-[700px] border-2 border-[#f5f5f5]">
-          <thead className="h-[50px] bg-[#f5f5f5] border-2 border-[#f5f5f5]">
-            <tr className="text-left">
-              <th>c</th>
-              <th
-                className={`${Pratafont.className} font-normal text-sm leading-[200%] uppercase text-[#080808]`}
-              >
-                Product
-              </th>
-              <th
-                className={`${Pratafont.className} font-normal text-sm leading-[200%] uppercase text-[#080808]`}
-              >
-                Quantity
-              </th>
-              <th
-                className={`${Pratafont.className} font-normal text-sm leading-[200%] uppercase text-[#080808]`}
-              >
-                price
-              </th>
-              <th
-                className={`${Pratafont.className} font-normal text-sm leading-[200%] uppercase text-[#080808]`}
-              >
-                Subtotal
-              </th>
-            </tr>
-          </thead>
-          <tbody className="border-2 border-[#f5f5f5] h-[132px]">
-            {products.map((data) => (
-              <>
-                <tr key={data.id}>
-                  <td>
-                    <div className="flex gap-2 items-center">
-                      <IoMdCloseCircleOutline />
-                      <Image
-                        className="w-[100px] h-[100px]"
-                        src={data.thumbnail}
-                        alt={data.thumbnail}
-                        width={100}
-                        height={100}
-                      />
-                    </div>
-                  </td>
-                  <td>
-                    <h3 className="font-normal text-[0.94rem] leading-5 text-[#080808]">
-                      {data.title.slice(0,20)}... 
-                    </h3>
-                  </td>
-                  <td>
-                    <div className="h-full border border-[#8F8F8F] inline-block ">
-                      <div className="flex cursor-pointer items-center h-full">
-                        <div
-                          onClick={() => handleminus()}
-                          className="border-r border-[#8F8F8F] h-full px-4 flex justify-center items-center"
-                        >
-                          <AiOutlineMinus />
-                        </div>
-                        <div className=" h-full px-7 flex justify-center items-center">
-                          <h3 className="font-normal text-xl leading-[200%] text-center text-[#666]">
-                            {data.count}
-                          </h3>
-                        </div>
-                        <div
-                          onClick={() => handleplus()}
-                          className="border-l border-[#8F8F8F] h-full px-4 flex justify-center items-center"
-                        >
-                          <AiOutlinePlus />
+        <div className="overflow-x-scroll">
+          <table className="w-[700px] border-2 border-[#f5f5f5]">
+            <thead className="h-[50px] bg-[#f5f5f5] border-2 border-[#f5f5f5]">
+              <tr className="text-left">
+                <th>c</th>
+                <th
+                  className={`${Pratafont.className} font-normal text-sm leading-[200%] uppercase text-[#080808]`}
+                >
+                  Product
+                </th>
+                <th
+                  className={`${Pratafont.className} font-normal text-sm leading-[200%] uppercase text-[#080808]`}
+                >
+                  Quantity
+                </th>
+                <th
+                  className={`${Pratafont.className} font-normal text-sm leading-[200%] uppercase text-[#080808]`}
+                >
+                  price
+                </th>
+                <th
+                  className={`${Pratafont.className} font-normal text-sm leading-[200%] uppercase text-[#080808]`}
+                >
+                  Subtotal
+                </th>
+              </tr>
+            </thead>
+            <tbody className="border-2 border-[#f5f5f5] h-[132px]">
+              {products.map((data) => (
+                <div key={data.id}>
+                  <tr>
+                    <td>
+                      <div className="flex gap-2 items-center">
+                        <IoMdCloseCircleOutline />
+                        <Image
+                          className="w-[100px] h-[100px]"
+                          src={data.thumbnail}
+                          alt={data.thumbnail}
+                          width={100}
+                          height={100}
+                        />
+                      </div>
+                    </td>
+                    <td>
+                      <h3 className="font-normal text-[0.94rem] leading-5 text-[#080808]">
+                        {data.title.slice(0, 20)}...
+                      </h3>
+                    </td>
+                    <td>
+                      <div className="h-full border border-[#8F8F8F] inline-block ">
+                        <div className="flex cursor-pointer items-center h-full">
+                          <div
+                            onClick={() => handleminus()}
+                            className="border-r border-[#8F8F8F] h-full px-4 flex justify-center items-center"
+                          >
+                            <AiOutlineMinus />
+                          </div>
+                          <div className=" h-full px-7 flex justify-center items-center">
+                            <h3 className="font-normal text-xl leading-[200%] text-center text-[#666]">
+                              {data.count}
+                            </h3>
+                          </div>
+                          <div
+                            onClick={() => handleplus()}
+                            className="border-l border-[#8F8F8F] h-full px-4 flex justify-center items-center"
+                          >
+                            <AiOutlinePlus />
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </td>
-                  <td>{data.price}</td>
-                  <td>{data.price}</td>
-                </tr>
-              </>
-            ))}
-          </tbody>
-        </table>
+                    </td>
+                    <td>{data.price}</td>
+                    <td>{data.price}</td>
+                  </tr>
+                </div>
+              ))}
+            </tbody>
+          </table>
+        </div>
         <div>page</div>
       </Container>
     </>
