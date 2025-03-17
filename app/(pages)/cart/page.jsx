2389d783f3ca3a7106446bdc8aa0db9e64cb6e82
akash100a <arakash968786@gmail.com>
2025-03-18 03:33:23 +0600

    Add quantity adjustment controls to Cart page

--- app/(pages)/cart/page.jsx
+++ app/(pages)/cart/page.jsx
@@ -4,6 +4,7 @@ import { Context } from "@/app/context/productContext";
 import { Prata } from "next/font/google";
 import Image from "next/image";
 import { useContext } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 import { FaAngleRight } from "react-icons/fa";
 import { IoMdCloseCircleOutline } from "react-icons/io";
 const Pratafont = Prata({
@@ -13,7 +14,8 @@ const Pratafont = Prata({
 const page = () => {
   let { products, setProduct } = useContext(Context);
   console.log(products);
-
+  let handleminus = () => {};
+  let handleplus = () => {};
   return (
     <>
       {/* Shop */}
@@ -66,10 +68,10 @@ const page = () => {
               <>
                 <tr key={data.id}>
                   <td>
-                    <div className="flex gap-2">
+                    <div className="flex gap-2 items-center">
                       <IoMdCloseCircleOutline />
                       <Image
-                      className="w-[100px] h-[100px]"
+                        className="w-[100px] h-[100px]"
                         src={data.thumbnail}
                         alt={data.thumbnail}
                         width={100}
@@ -82,7 +84,29 @@ const page = () => {
                       {data.title}
                     </h3>
                   </td>
-                  <td>c</td>
+                  <td>
+                    <div className="h-full border border-[#8F8F8F] inline-block ">
+                      <div className="flex cursor-pointer items-center h-full">
+                        <div
+                          onClick={() => handleminus()}
+                          className="border-r border-[#8F8F8F] h-full px-4 flex justify-center items-center"
+                        >
+                          <AiOutlineMinus />
+                        </div>
+                        <div className=" h-full px-7 flex justify-center items-center">
+                          <h3 className="font-normal text-xl leading-[200%] text-center text-[#666]">
+                            {data.count}
+                          </h3>
+                        </div>
+                        <div
+                          onClick={() => handleplus()}
+                          className="border-l border-[#8F8F8F] h-full px-4 flex justify-center items-center"
+                        >
+                          <AiOutlinePlus />
+                        </div>
+                      </div>
+                    </div>
+                  </td>
                   <td>{data.price}</td>
                   <td>{data.price}</td>
                 </tr>
