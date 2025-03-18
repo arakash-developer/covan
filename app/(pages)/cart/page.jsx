fba1b5bbb3de10e1aae36915edf64ad358d57433
akash100a <arakash968786@gmail.com>
2025-03-18 06:00:14 +0600

    Refactor Cart table structure for improved readability and maintainability

--- app/(pages)/cart/page.jsx
+++ app/(pages)/cart/page.jsx
@@ -4,16 +4,15 @@ import { Context } from "@/app/context/productContext";
 import { Prata } from "next/font/google";
 import Image from "next/image";
 import { useContext } from "react";
-import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 import { FaAngleRight } from "react-icons/fa";
 import { IoMdCloseCircleOutline } from "react-icons/io";
 const Pratafont = Prata({
   weight: "400",
   subsets: ["latin"],
 });
+
 const page = () => {
   let { products, setProduct } = useContext(Context);
-  console.log(products);
   let handleminus = () => {};
   let handleplus = () => {};
   return (
@@ -37,9 +36,9 @@ const page = () => {
       </section>
       {/* Shop */}
       <Container className="mt-[110px] mb-[150px] flex md:flex-row flex-col justify-between items-start">
-        <div className="w-full overflow-x-scroll">
+        <div className="w-full overflow-x-scroll md:overflow-auto">
           <table className="w-[700px] border-2 border-[#f5f5f5]">
-            <thead className="w-full h-[50px] bg-[#f5f5f5] border-2 border-[#f5f5f5]">
+            <thead className="h-[50px] bg-[#f5f5f5] border-2 border-[#f5f5f5]">
               <tr className="text-left">
                 <th>c</th>
                 <th
@@ -66,52 +65,28 @@ const page = () => {
             </thead>
             <tbody className="border-2 border-[#f5f5f5] h-[132px]">
               {products.map((data) => (
-                <div key={data.id}>
-                  <tr>
-                    <td>
-                      <div className="flex gap-2 items-center">
-                        <IoMdCloseCircleOutline />
-                        <Image
-                          className="w-[100px] h-[100px]"
-                          src={data.thumbnail}
-                          alt={data.thumbnail}
-                          width={100}
-                          height={100}
-                        />
-                      </div>
-                    </td>
-                    <td>
-                      <h3 className="font-normal text-[0.94rem] leading-5 text-[#080808]">
-                        {data.title.slice(0, 20)}...
-                      </h3>
-                    </td>
-                    <td>
-                      <div className="h-full border border-[#8F8F8F] inline-block ">
-                        <div className="flex cursor-pointer items-center h-full">
-                          <div
-                            onClick={() => handleminus()}
-                            className="border-r border-[#8F8F8F] h-full px-4 flex justify-center items-center"
-                          >
-                            <AiOutlineMinus />
-                          </div>
-                          <div className=" h-full px-7 flex justify-center items-center">
-                            <h3 className="font-normal text-xl leading-[200%] text-center text-[#666]">
-                              {data.count}
-                            </h3>
-                          </div>
-                          <div
-                            onClick={() => handleplus()}
-                            className="border-l border-[#8F8F8F] h-full px-4 flex justify-center items-center"
-                          >
-                            <AiOutlinePlus />
-                          </div>
-                        </div>
-                      </div>
-                    </td>
-                    <td>{data.price}</td>
-                    <td>{data.price}</td>
-                  </tr>
-                </div>
+                <tr key={data.id}>
+                  <td className="flex gap-2 items-center">
+                    <IoMdCloseCircleOutline />
+                    <Image
+                      className="w-[100px] h-[100px]"
+                      src={data.thumbnail}
+                      alt={data.thumbnail}
+                      width={100}
+                      height={100}
+                    />
+                  </td>
+                  <td>
+                    <h3 className="font-normal text-[0.94rem] leading-5 text-[#080808]">
+                      {data.title.slice(0, 20)}...
+                    </h3>
+                  </td>
+                  <td>
+                    <span>ss</span>
+                  </td>
+                  <td>{data.price}</td>
+                  <td>{data.price}</td>
+                </tr>
               ))}
             </tbody>
           </table>
