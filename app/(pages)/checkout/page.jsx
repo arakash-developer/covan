a70a5cebb73d1a21616eb52b6dd1c02fdb6cf508
akash100a <arakash968786@gmail.com>
2025-03-19 21:05:43 +0600

    Fix label attributes and update country/region input to a dropdown in checkout form

--- app/(pages)/checkout/page.jsx
+++ app/(pages)/checkout/page.jsx
@@ -12,7 +12,7 @@ const page = () => {
           <div className="item">
             <div className="flex items-center gap-[2px]">
               <label
-                htmlFor="floating_email"
+                htmlhtmlFor="floating_email"
                 className="font-normal text-md leading-[175%] capitalize"
               >
                 First name
@@ -33,7 +33,7 @@ const page = () => {
           <div className="item">
             <div className="flex items-center gap-[2px]">
               <label
-                htmlFor="floating_email"
+                htmlhtmlFor="floating_email"
                 className="font-normal text-md leading-[175%] capitalize"
               >
                 Last name
@@ -54,7 +54,7 @@ const page = () => {
           <div className="item">
             <div className="">
               <label
-                htmlFor="floating_email"
+                htmlhtmlFor="floating_email"
                 className="font-normal text-md leading-[175%] capitalize"
               >
                 Company name (optional)
@@ -70,22 +70,27 @@ const page = () => {
             />
           </div>
           <div className="item">
-            <div className="">
+            <div className="flex items-center gap-[2px]">
               <label
-                htmlFor="floating_email"
+                htmlhtmlFor="floating_email"
                 className="font-normal text-md leading-[175%] capitalize"
               >
                 Country/Region
               </label>
+              <span className="text-[#e42234] font-normal text-md leading-[175%] inline-block underline">
+                *
+              </span>
             </div>
-            <input
-              type="text"
-              name="floating_email"
-              id="floating_email"
-              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-[#ddd] appearance-none  focus:outline-none focus:ring-0 focus:border-[#e7b053] peer"
-              placeholder=" "
-              required
-            />
+            <select
+              className="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-[#ddd] appearance-none focus:outline-none focus:ring-0 focus:border-[#e7b053] peer cursor-pointer"
+              defaultValue="bangladesh"
+            >
+              <option value="bangladesh">Bangladesh</option>
+              <option value="US">United States</option>
+              <option value="CA">Canada</option>
+              <option value="FR">France</option>
+              <option value="DE">Germany</option>
+            </select>
           </div>
         </form>
       </Container>
