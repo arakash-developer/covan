1b03ede770f41c8d0b08fa7272c9db6a8b46c7c5
akash100a <arakash968786@gmail.com>
2025-03-19 20:53:37 +0600

    Update checkout form to include first name, last name, and optional company name fields

--- app/(pages)/checkout/page.jsx
+++ app/(pages)/checkout/page.jsx
@@ -5,21 +5,67 @@ const page = () => {
   return (
     <section className="my-[110px]">
       <Container>
-        <form className="w-full">
-          <label
-            htmlFor="floating_email"
-            className="font-normal text-md leading-[175%]"
-          >
-            Email address
-          </label>
-          <input
-            type="email"
-            name="floating_email"
-            id="floating_email"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-[#e7b053] peer"
-            placeholder=" "
-            required
-          />
+        <form className="w-full flex flex-col gap-y-[30px]">
+          <div className="item">
+            <div className="flex items-center gap-[2px]">
+              <label
+                htmlFor="floating_email"
+                className="font-normal text-md leading-[175%] capitalize"
+              >
+                First name
+              </label>
+              <span className="text-[#e42234] font-normal text-md leading-[175%] inline-block underline">
+                *
+              </span>
+            </div>
+            <input
+              type="text"
+              name="floating_email"
+              id="floating_email"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-[#ddd] appearance-none  focus:outline-none focus:ring-0 focus:border-[#e7b053] peer"
+              placeholder=" "
+              required
+            />
+          </div>
+          <div className="item">
+            <div className="flex items-center gap-[2px]">
+              <label
+                htmlFor="floating_email"
+                className="font-normal text-md leading-[175%] capitalize"
+              >
+                Last name
+              </label>
+              <span className="text-[#e42234] font-normal text-md leading-[175%] inline-block underline">
+                *
+              </span>
+            </div>
+            <input
+              type="text"
+              name="floating_email"
+              id="floating_email"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-[#ddd] appearance-none  focus:outline-none focus:ring-0 focus:border-[#e7b053] peer"
+              placeholder=" "
+              required
+            />
+          </div>
+          <div className="item">
+            <div className="">
+              <label
+                htmlFor="floating_email"
+                className="font-normal text-md leading-[175%] capitalize"
+              >
+                Company name (optional)
+              </label>
+            </div>
+            <input
+              type="text"
+              name="floating_email"
+              id="floating_email"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-[#ddd] appearance-none  focus:outline-none focus:ring-0 focus:border-[#e7b053] peer"
+              placeholder=" "
+              required
+            />
+          </div>
         </form>
       </Container>
     </section>
