abd0d6c46e602ba88d10be74639ef477cb38bab3
akash100a <arakash968786@gmail.com>
2025-03-26 13:09:27 +0700

    Refactor MyLi component to simplify class structure and enhance dropdown visibility on hover

--- app/component/layers/MyLi.jsx
+++ app/component/layers/MyLi.jsx
@@ -156,7 +156,7 @@ const MyLi = ({
     },
   ];
   return (
-    <li className="group h-full w-full md:w-auto">
+    <li className="group w-full md:w-auto">
       <Link
         className="flex items-center gap-1 font-normal text-[15px] leading-[109%] uppercase text-[#080808] hover:text-[#e7b053] relative after:w-0 after:h-[2px] after:absolute after:left-0 after:bottom-[-5px] after:bg-[#e7b053] after:transition-all after:ease-linear after:duration-[0.3s] hover:after:w-full md:w-auto justify-between md:justify-start py-5 md:py-0 border-b-2 md:border-0 w-full"
         href={link}
@@ -167,8 +167,8 @@ const MyLi = ({
         </span>
       </Link>
 
-      <div className="dropdown  md:w-[96%] bg-slate-100 md:absolute left-0 md:left-1/2 md:-translate-x-1/2 top-0 md:top-[100%] z-[999999] invisible opacity-0 group-hover:visible group-hover:opacity-100 transition-all duration-300 ease-linear px-10 bg-sale bg-cover bg-top bg-no-repeat py-10 md:overflow-y-scroll md:max-h-[80vh] no-scrollbar h-0 md:h-auto">
-        <div className="md:h-[100px] h-0 group-hover:h-full"></div>
+      <div className="dropdown md:w-[96%] bg-slate-100 md:absolute left-0 md:left-1/2 md:-translate-x-1/2 top-0 md:top-[100%] z-[999999] invisible opacity-0 group-hover:visible group-hover:opacity-100 transition-all duration-300 ease-linear px-10 bg-sale bg-cover bg-top bg-no-repeat md:py-10 md:overflow-y-scroll md:max-h-[80vh] no-scrollbar h-0 md:h-auto">
+        <div className="md:h-[100px] h-0"></div>
         <div className="mt-5 py-5 w-full grid md:grid-cols-3 lg:grid-cols-6 items-start gap-4 justify-between ">
           {categoryList.map((category) => (
             <div key={category.id}>
